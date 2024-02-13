5c1761c76fcccd7fc0bfd9c1688ae1c13785805d
andres.mera <feli041089@gmail.com>
2024-02-13 14:38:54 -0500

    master : contador

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,12 @@ export class AppComponent {
   public title: string = 'Hola mundo';
   public subtitle: string = 'Mi primera app en Angular';
   public counter: number = 10;
+
+  increaseBy(value: number): void {
+    this.counter += value;
+  }
+
+  resetCounter(): void {
+    this.counter = 10;
+  }
 }
