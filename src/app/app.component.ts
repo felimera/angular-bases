0627549a3b5ae2830de75abfd11ee5a655ab6159
andres.mera <feli041089@gmail.com>
2024-02-13 15:01:56 -0500

    master : Funcionalidad del contador

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,4 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public title: string = 'Hola mundo';
   public subtitle: string = 'Mi primera app en Angular';
-  public counter: number = 10;
-
-  increaseBy(value: number): void {
-    this.counter += value;
-  }
-
-  resetCounter(): void {
-    this.counter = 10;
-  }
 }
--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,9 +1,26 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
-  template: '<h1>Hola Counter</h1>'
-})
-export class CounterComponent {
+  template: `
+    <h3>Counter : {{counter}}</h3>
+    <p>Bienvenidos a Angular</p>
 
+    <button (click)="increaseBy(1)">+1</button>
+    <button (click)="resetCounter()">Reset</button>
+    <button (click)="increaseBy(-1)">-1</button>
+  `
+})
+
+export class CounterComponent {
+  public counter: number = 10;
+
+  increaseBy(value: number): void {
+    this.counter += value;
+  }
+
+  resetCounter(): void {
+    this.counter = 10;
+  }
+  constructor() { }
 }
