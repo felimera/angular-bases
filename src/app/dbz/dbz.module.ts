943e05624889326cc809da37fd458ac4812f7152
andres.mera <feli041089@gmail.com>
2024-02-14 14:49:33 -0500

    master : Pensemos en componentes pequeños.

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,18 +1,20 @@
-import { NgModule  } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './pages/main-page.component';
+import { ListComponent } from './components/list/list.component';
 
 
 
 @NgModule({
   declarations: [
-    MainPageComponent
+    MainPageComponent,
+    ListComponent
   ],
-  exports:[
+  exports: [
     MainPageComponent
   ],
   imports: [
-    CommonModule
+    // CommonModule
   ]
 })
 export class DbzModule { }
