32833fd17d6e5b2c1beb992265670a68cc1cf6de
andres.mera <feli041089@gmail.com>
2024-02-15 08:30:28 -0500

    master : FormsModule y ngModel

--- src/app/dbz/components/add-character/add-character.component.ts
+++ src/app/dbz/components/add-character/add-character.component.ts
@@ -1,9 +1,20 @@
 
 import { Component } from '@angular/core';
+import { Character } from '../../interfaces/character.interfaces';
 
 @Component({
   selector: 'app-dbz-add-character',
   templateUrl: './add-character.component.html',
   styleUrl: './add-character.component.css',
 })
-export class AddCharacterComponent { }
+export class AddCharacterComponent {
+
+  public character: Character = {
+    name: '',
+    power: 0
+  };
+
+  emitCharacter(): void {
+    console.log(this.character);
+  }
+}
